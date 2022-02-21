1526928fdbe95e8ccb54d555875fd2717fec4a5e
sammy ocharo <obanyisammy@gmail.com>
2022-02-22 00:29:56 +0300

    coffee website v1

--- coffeeBackend/settings.py
+++ coffeeBackend/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
