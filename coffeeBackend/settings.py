31640cdd18613f9142089573eab80ff453b52040
sammy ocharo <obanyisammy@gmail.com>
2022-02-22 15:15:58 +0300

    deploy version 1

--- coffeeBackend/settings.py
+++ coffeeBackend/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # Default primary key field type
